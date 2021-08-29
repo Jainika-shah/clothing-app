1f4d485bcdcb9d95ae5667ceaa77d81023d8962c
Jainikashah <jainikashah3@gmail.com>
2021-08-29 19:23:06 +0530

    Initial Commit

---
+++ src/homePageComponent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './homePageStyles.css'
+
+function HomePage(){
+    return <div>
+        <div className="menu-container">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">Hats</h1>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">Hats</h1>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">Hats</h1>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">Hats</h1>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="menu-title">Hats</h1>
+                    <p>Shop Now</p>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+
+}
+export default HomePage
